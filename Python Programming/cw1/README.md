2cd3e25f98649ee51ba0263c72d82dc76bed60dc
dingke <44361803+realdingke@users.noreply.github.com>
2021-07-07 16:47:39 +0800

    Add files via upload

---
+++ Python Programming/cw1/README.md
@@ -0,0 +1,7 @@
+## CO558 Python Programming: Coursework 1 (Battleship)
+
+### Introduction
+
+This repository contains the skeleton code that you need 
+in order to complete the coursework.
+
---
+++ Python Programming/cw1/main.py
@@ -0,0 +1,8 @@
+from examples_scripts import *
+
+if __name__ == '__main__':
+    #example_two_players_users()
+    #example_user_manual_board_vs_full_automatic()
+    #example_user_automatic_board_vs_full_automatic()
+    full_automatic_vs_full_automatic()
+    #random_vs_random()
